83f19ac8634731c97ea61d1c4af280fa7861c640
dany_ <dany_@DESKTOP-IIOKRK5>
2017-04-06 11:11:25 -0700

    web

---
+++ candidatas-library/Webcandi2017/Controllers/CandidataController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using SiCandi2017.Controlador;
+
+
+
+namespace SisCandidatasWeb_2017.Controllers
+{
+    public class CandidataController : Controller
+    {
+        // GET: Candidata
+        public ActionResult Index(string word = "", string m = "")
+        {
+            ViewBag.DatosC = ManejoCandidata.Buscar(word,true, m);
+            ViewBag.DatosM = ManejoMunicipio.getAll(true);
+            ViewBag.word = word;
+            return View();
+        }
+
+        public ActionResult Like(int pkCandidata)
+        {
+            ManejoCandidata.Like(pkCandidata);
+            return RedirectToAction("");
+        }
+    }
+}
---
+++ candidatas-library/Webcandi2017/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SisCandidatasWeb_2017.MvcApplication" Language="C#" %>
